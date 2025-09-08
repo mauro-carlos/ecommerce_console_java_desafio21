e802514fca552d4bd70c3581b99e939ad8e5395b
Mauro Carlos <mauro_carlos14@hotmail.com>
2025-09-08 13:16:30 -0400

    criando variaveis primitivas

--- src/main/java/br/com/didox/App.java
+++ src/main/java/br/com/didox/App.java
@@ -1,13 +1,22 @@
 package br.com.didox;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.Scanner;
+
+public class App {
+static Scanner scanner = new Scanner(System.in);
+    
+
+    public static void main(String[] args) {
+        
+int numero1 = 3;
+var numero2 = 20;
+        
+       
+
+        System.out.printf("o numero é: %s\n",numero1);
+        System.out.printf("A soma do %s e do %s = %s\n", numero1,numero2,numero1+numero2);
     }
+
+    
+
 }
